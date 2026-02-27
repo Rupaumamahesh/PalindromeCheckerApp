d3bdbbf671c20c08da24508919509b659f69816c
Rupa Mahesh <rupaumamahes@gmail.com>
2026-02-27 15:09:28 +0530

    [Rupa Mahesh] Added Usecase 1 PalindromeChecker

---
+++ palindromecheckerapp.java
@@ -0,0 +1,9 @@
+package PalindromeCheckerApp;
+
+public class palindromecheckerapp {
+    public static void main(String[]args) {
+        System.out.println("Welcome to the Palindrome Checker Management");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
