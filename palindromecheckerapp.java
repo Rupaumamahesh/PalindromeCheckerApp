dd31d039c107ee3e299fce940f79bade1eb44fbe
Rupa Mahesh <rupaumamahes@gmail.com>
2026-03-08 16:51:02 +0530

    Rupa Added UC4-CharacterArrayPalindrome

--- palindromecheckerapp.java
+++ palindromecheckerapp.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 
 public class palindromecheckerapp {
     public static void main(String[]args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        // Take user input
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
 
-        String reversed = "";
-
-        // Reverse the string
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is NOT a Palindrome.");
